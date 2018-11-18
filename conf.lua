f423708eb27541e3e4ed7685d1f933a4199babac
Dalton <daltonsmith45@hotmail.com>
2018-11-17 19:09:00 -0600

    Added config

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.title = "..."
+    t.window.icon = nil                 
+    t.window.width = 1024                
+    t.window.height = 800               
+    t.modules.joystick = false
+end
